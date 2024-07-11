6eca6e9ebe04b67805aa02b81412a6bc01eb7a8c
tapas <tapas@pizzapatapa.me>
2024-07-11 16:44:53 +0200

    Update run benchmark script

--- run_benchmarks_v2_0.sh
+++ run_benchmarks_v2_0.sh
@@ -2,39 +2,33 @@ PYTHON=${PY:="python3 -u"}
 PARALLEL=${PARALLELISM:=1}
 GISTPARALLEL=${GISTPARALLELISM:=$PARALLEL}
 
-# K=10 runs
-ALGODEF=custom_definitions
+ALGODEF=${DEF:=custom_definitions_first_run}
 
 start=`date +%s`
 echo Started benchmark at `date`
 
 mkdir -p logs
 
-for ds in glove-50-angular glove-100-angular glove-200-angular sift-128-euclidean nytimes-256-angular nytimes-16-angular fashion-mnist-784-euclidan; do
-  for algo in pgvector-hnsw vespa-hnsw pgvector-hnsw pgvector-ivfflat elasticsearch-hnsw weaviate-hnsw qdrant-hnsw redisearch-hnsw; do
-      $PYTHON run.py --dataset $ds --definitions $ALGODEF --runs 3 --count 10 --parallelism $PARALLEL --algorithm $algo
-      cp annb.log logs/${ds}_10_all.log
+for k in 2 4 10 20 50 100; do
+  for ds in glove-50-angular glove-100-angular glove-200-angular sift-128-euclidean nytimes-256-angular nytimes-16-angular fashion-mnist-784-euclidan; do
+    for algo in vespa-hnsw pgvector-hnsw pgvector-ivfflat elasticsearch-hnsw weaviate-hnsw qdrant-hnsw redisearch-hnsw; do
+        $PYTHON run.py --dataset $ds --definitions $ALGODEF --runs 3 --count $k --parallelism $PARALLEL --algorithm $algo
+        cp annb.log logs/${ds}_${k}_parallel_${PARALLEL}.log
+    done
+    # separate milvus runs because problems with parallelism
+    for algo in milvus-flat milvus-ivfflat milvus-ivfsq8 milvus-ivfpq milvus-hnsw milvus-scann; do
+      $PYTHON run.py --dataset $ds --definitions $ALGODEF --runs 3 --count $k --parallelism 1 --algorithm $algo
+      cp annb.log logs/${ds}_${k}_parallel_1.log
+    done
   done
-done
-
-# separate milvus runs because problems with parallelism
-for ds in glove-50-angular glove-100-angular glove-200-angular sift-128-euclidean nytimes-256-angular nytimes-16-angular fashion-mnist-784-euclidan; do
   for algo in milvus-flat milvus-ivfflat milvus-ivfsq8 milvus-ivfpq milvus-hnsw milvus-scann; do
-      $PYTHON run.py --dataset $ds --definitions $ALGODEF --runs 3 --count 10 --parallelism 1 --algorithm $algo
-      cp annb.log logs/${ds}_10_milvus.log
+    $PYTHON run.py --dataset gist-960-euclidean --definitions $ALGODEF --runs 1 --count $k --parallelism 1 --algorithm $algo
+    cp annb.log logs/gist-960-euclidean_${k}_parallel_1.log
   done
-done
-
-# separate GIST runs because of high RAM usage
-for algo in pgvector-hnsw vespa-hnsw pgvector-hnsw pgvector-ivfflat elasticsearch-hnsw wewaviate-hnsw qdrant-hnsw redisearch-hnsw; do
-  $PYTHON run.py --dataset gist-960-euclidean --definitions $ALGODEF --runs 1 --count 10 --parallelism $GISTPARALLEL --algorithm $algo
-  cp annb.log logs/gist-960-euclidean_10_all.log
-done
-
-# separate milvus runs because problems with parallelism
-for algo in milvus-flat milvus-ivfflat milvus-ivfsq8 milvus-ivfpq milvus-hnsw milvus-scann; do
-  $PYTHON run.py --dataset gist-960-euclidean --definitions $ALGODEF --runs 1 --count 10 --parallelism 1 --algorithm $algo
-  cp annb.log logs/gist-960-euclidean_10_milvus.log
+  for algo in vespa-hnsw pgvector-hnsw pgvector-ivfflat elasticsearch-hnsw weaviate-hnsw qdrant-hnsw redisearch-hnsw; do
+      $PYTHON run.py --dataset gist-960-euclidean --definitions $ALGODEF --runs 1 --count $k --parallelism $GISTPARALLEL --algorithm $algo
+      cp annb.log logs/gist-960-euclidean_${k}_parallel_${GISTPARALLEL}.log
+  done
 done
 
 end=`date +%s`
