41ed0df0163cc9dd4db33f8875f2655112fb14c2
tapas <tapas@pizzapatapa.me>
2024-07-25 11:29:47 +0200

    Add a script to plot the results of all datasets with the appropriate configurations

---
+++ plot_all_results.sh
@@ -0,0 +1,12 @@
+X=${XAXIS:="k-nn"}
+Y=${YAXIS:="qps"}
+XS=${XSCALE:="linear"}
+YS=${YSCALE:="linear"}
+
+for ds in glove-50-angular glove-100-angular glove-200-angular sift-128-euclidean; do
+  mkdir -p results/${ds}_plots
+  for count in 2 4 10 20 50 100; do
+      python3 plot.py --dataset $ds --count $count -x $X -y $Y -X $XS -Y $YS \
+       -o results/${ds}_plots/${ds}_x_${X}_y_${Y}_xs_${XS}_ys_${YS}_k_${count}.png
+  done
+done
