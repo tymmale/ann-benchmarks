50fb00435bd73e78532263a2874884210e5a3108
Tammy Lessig <tammy@tammy.informatik.hs-furtwangen.de>
2024-07-29 15:14:47 +0200

    Update which results should be plotted

--- plot_all_results.sh
+++ plot_all_results.sh
@@ -3,7 +3,7 @@ Y=${YAXIS:="qps"}
 XS=${XSCALE:="linear"}
 YS=${YSCALE:="linear"}
 
-for ds in glove-50-angular glove-100-angular glove-200-angular sift-128-euclidean; do
+for ds in glove-50-angular glove-100-angular sift-128-euclidean nytimes-256-angular fashion-mnist-784-euclidean; do
   mkdir -p results/${ds}_plots
   for count in 2 4 10 20 50 100; do
       python3 plot.py --dataset $ds --count $count -x $X -y $Y -X $XS -Y $YS \
