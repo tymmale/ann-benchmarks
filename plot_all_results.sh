400cf95de5bcd959f1d0025f9f8c20ddcd6bd5ed
tapas <tapas@pizzapatapa.me>
2024-08-06 18:27:23 +0200

    Modify storage location of the plots for a better overview and organisation

--- plot_all_results.sh
+++ plot_all_results.sh
@@ -5,8 +5,9 @@ YS=${YSCALE:="linear"}
 
 for ds in glove-50-angular glove-100-angular sift-128-euclidean nytimes-256-angular fashion-mnist-784-euclidean; do
   mkdir -p results/${ds}_plots
+  mkdir -p results/${ds}_plots/${X}_${XS}_${Y}_${YS}
   for count in 2 4 10 20 50 100; do
       python3 plot.py --dataset $ds --count $count -x $X -y $Y -X $XS -Y $YS \
-       -o results/${ds}_plots/${ds}_x_${X}_y_${Y}_xs_${XS}_ys_${YS}_k_${count}.png
+       -o results/${ds}_plots/${X}_${XS}_${Y}_${YS}/${ds}_x_${X}_y_${Y}_xs_${XS}_ys_${YS}_k_${count}.png
   done
 done
