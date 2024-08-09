3e806d08b20f992e8500f19510fb47c36afae2c4
tapas <tapas@pizzapatapa.me>
2024-08-09 22:50:27 +0200

    Throw a value error to ensure that it gets properly handled

--- ann_benchmarks/algorithms/milvus/module.py
+++ ann_benchmarks/algorithms/milvus/module.py
@@ -215,7 +215,8 @@ class MilvusIVFPQ(Milvus):
         self._index_nbits = index_param.get("nbits", None)
 
     def get_index_param(self):
-        assert self._dim % self._index_m == 0, "dimension must be able to be divided by m"
+        if self._dim % self._index_m == 0:
+            raise ValueError("dimension must be able to be divided by m")
         return {
             "index_type": "IVF_PQ",
             "params": {
