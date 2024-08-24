307b6463888e7d78f06144d3052aee768e72e66b
tapas <tapas@pizzapatapa.me>
2024-08-25 00:34:49 +0200

    Plot the raw results

--- plot_all_results_by_algorithm.sh
+++ plot_all_results_by_algorithm.sh
@@ -7,6 +7,6 @@ DATASETS=()
 
 mkdir -p results/by_algorithm/${X}_${XS}_${Y}_${YS}
 for count in 2 4 10 20 50 100; do
-    python3 plot_by_algorithm.py --dataset glove-50-angular glove-100-angular nytimes-256-angular sift-128-euclidean fashion-mnist-784-euclidean \
-     --count $count -x $X -y $Y -X $XS -Y $YS -o results/by_algorithm/${X}_${XS}_${Y}_${YS}/x_${X}_y_${Y}_xs_${XS}_ys_${YS}_k_${count}
+    python3 plot_by_algorithm.py --raw --dataset glove-50-angular glove-100-angular nytimes-256-angular sift-128-euclidean fashion-mnist-784-euclidean \
+     --count $count -x $X -y $Y -X $XS -Y $YS -o results/by_algorithm/${X}_${XS}_${Y}_${YS}/benchmark_v_4_0_x_${X}_y_${Y}_xs_${XS}_ys_${YS}_k_${count}_raw
 done
